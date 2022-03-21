dc06da380af4db2c7d09c49ea49f047f1f78a1c2
Gyobeom <ikiscc@gmail.com>
2022-03-21 11:09:15 +0900

    Initial commit

---
+++ chapter2.py
@@ -0,0 +1,16 @@
+#함수 return 이용시 아무런 값도 리턴하지 않는경우 NONE으로 전달됨
+# def test(name):
+#     print('hi' + name)
+#     return
+#
+# print(test('brian'))
+
+
+def minus(a, b):
+    """두 수의 차를 구하는 함수"""
+    return a - b
+
+print(minus(5,3))
+
+help(minus)
+
